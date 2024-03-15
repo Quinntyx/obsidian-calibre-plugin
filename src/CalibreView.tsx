ca0f090f5cf78665161ca121b3bb9564d209159d
Quinntyx <harrisonhenry916@gmail.com>
2024-03-15 12:33:33 -0500

    added my token (it's local only dw)

--- src/CalibreView.tsx
+++ src/CalibreView.tsx
@@ -20,11 +20,21 @@ export class CalibreView extends ItemView {
   async onOpen() {
     const container = this.containerEl.children[1];
     try {
-      const iframe = container.createEl('iframe');
-      iframe.setAttribute('sandbox', 'allow-forms allow-presentation allow-same-origin allow-scripts allow-modals allow-downloads allow-popups');
-      iframe.src = this.settings.replaceLocalIp
+      const res = await fetch(this.settings.replaceLocalIp
         ? this.settings.address.replace('localhost', ip.address())
-        : this.settings.address;
+        : this.settings.address, {
+        method: 'GET',
+        headers: {
+          remember_token: "1|227fb89fe72afd0bf4b5d09989e4772ac04dabd936de2468b8158f00406c7e6fd974fddfa53c2e9e81d02169f4e871e48991bbf3915f9bafc37984b2e24f79db;"
+        }
+      });
+      const blob = await res.blob();
+      const urlObject = URL.createObjectURL(blob);
+
+      const iframe = container.createEl('iframe');
+
+      iframe.setAttribute('sandbox', 'allow-forms allow-presentation allow-same-origin allow-scripts allow-modals allow-downloads allow-popups');
+      iframe.src = urlObject;
     } catch (e) {
       console.error(e);
       const error = container.createDiv({ text: e.toString() });
